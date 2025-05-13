0bd6311929d2c3910f3c8372fd29ae6cf61dc561
amyyisdead <754989@student.dusd.net>
2025-05-12 20:50:43 -0700

    lab 3

---
+++ Lab2PerezP4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private Rigidbody playerRb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        playerRb.AddForce(movement * speed);
+
+
+        
+    }
+}
